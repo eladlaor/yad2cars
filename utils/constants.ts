772648f791b336644e819c6e4fca4260a489b6aa
Elad Laor <eladlaor88@gmail.com>
2024-06-02 21:47:37 +0300

    3 max retries

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 export const YAD2_CAR_SEARCH_URL = "https://www.yad2.co.il/vehicles/cars";
 
-export const MAX_RETRIES = 1;
+export const MAX_RETRIES = 3;
 
 export const EXAMPLES_DATASET_FILE_ID = "file-o2FDoVZXK6ogyd6idAppCsrW";
 
