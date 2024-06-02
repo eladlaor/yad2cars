165a97c1771bba09f2394a21299dc8093e45494c
Elad Laor <eladlaor88@gmail.com>
2024-06-02 19:40:06 +0300

    finetune job creation script

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,7 @@
 export const YAD2_CAR_SEARCH_URL = "https://www.yad2.co.il/vehicles/cars";
 
 export const MAX_RETRIES = 3;
+
+export const EXAMPLES_DATASET_FILE_ID = "file-iCwOHgwW8qNNpWUJkuwqGCJs";
+
+export const FINE_TUNE_JOB_ID = "ftjob-83nPaTIr9Kdu0xsBwwtfe46K";
