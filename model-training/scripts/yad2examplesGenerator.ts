4620774cc1fd444e24e68cda204740bd8ad62125
Elad Laor <eladlaor88@gmail.com>
2024-06-02 18:48:44 +0300

    better script for a better dataset

--- model-training/scripts/yad2examplesGenerator.ts
+++ model-training/scripts/yad2examplesGenerator.ts
@@ -42,25 +42,52 @@ const generateExample = (): {
       : Math.floor(Math.random() * (100000 - priceStart + 1)) + priceStart
     : null;
 
-  const userPrompt = `מחפש ${carFamilyTypes.join(" ")} ${manufacturers
-    .map((m) => m.title)
-    .join(" ")}${
-    yearStart
-      ? ` בין השנים ${yearStart} ל-${yearEnd}`
-      : yearEnd
-      ? ` עד שנה ${yearEnd}`
-      : ""
-  }${priceStart ? ` במחיר של ${priceStart} עד ${priceEnd}` : ""}`;
+  const promptVariations = ["מחפש", "מעוניין ב", "רוצה למצוא", "צריך"];
+  let userPrompt = getRandomElement(promptVariations);
+
+  if (carFamilyTypes.length > 0) {
+    userPrompt += ` ${carFamilyTypes.join(" ")}`;
+  }
+  if (manufacturers.length > 0) {
+    userPrompt += ` ${manufacturers.map((m) => m.title).join(" ")}`;
+  }
+  if (yearStart || yearEnd) {
+    if (yearStart && yearEnd) {
+      userPrompt += ` בין השנים ${yearStart} ל-${yearEnd}`;
+    } else if (yearStart) {
+      userPrompt += ` משנת ${yearStart}`;
+    } else if (yearEnd) {
+      userPrompt += ` עד שנה ${yearEnd}`;
+    }
+  }
+  if (priceStart || priceEnd) {
+    if (priceStart && priceEnd) {
+      userPrompt += ` במחיר של ${priceStart} עד ${priceEnd}`;
+    } else if (priceStart) {
+      userPrompt += ` במחיר של לפחות ${priceStart}`;
+    } else if (priceEnd) {
+      userPrompt += ` במחיר של עד ${priceEnd}`;
+    }
+  }
 
   const assistantResponse: ResponseKeys = {
-    carFamilyType: carFamilyTypes.length
-      ? carFamilyTypes.map((type) => carFamilyTypeMapping[type].toString())
-      : null,
+    carFamilyType:
+      carFamilyTypes.length > 0
+        ? carFamilyTypes.map((type) => carFamilyTypeMapping[type].toString())
+        : null,
     manufacturer:
-      manufacturers.length > 0 ? manufacturers.map((m) => m.id) : null,
+      manufacturers.length > 0
+        ? manufacturers.map((m) => m.id.toString())
+        : null,
     year: yearStart ? `${yearStart}-${yearEnd || "-1"}` : null,
     price: priceStart ? `${priceStart}-${priceEnd || "-1"}` : null,
   };
+
+  userPrompt = userPrompt
+    .replace(/ עד -$/, "")
+    .replace(/ ל-$/, "")
+    .replace(/  +/g, " ")
+    .trim();
 
   return { userPrompt, assistantResponse };
 };
